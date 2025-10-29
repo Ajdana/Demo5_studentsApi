d3197e96a38dd6932adda7992c0892dd8b69bba2
Aidana <ajdanaamirtaj@gmail.com>
2025-10-29 15:02:09 +0500

    Soft Delete

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes;//мягкое удаление
 
     //калонки разрешенные к заполнению
     protected $fillable = [
--- database/migrations/2025_10_29_044531_create_students_table.php
+++ database/migrations/2025_10_29_044531_create_students_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('avatar_url')->nullable();
             $table->timestamps();
-            $table->softDeletes();
+            $table->softDeletes();//мягкое удаление
         });
     }
 
